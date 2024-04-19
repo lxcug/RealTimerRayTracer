e4d09573df8c0ac1c4f94212aa2c7248845ad29f
lxcug <cugluxu@gmail.com>
2024-04-19 18:48:44 +0800

    Test Code

---
+++ RayTracer/Samples/CreateScene.h
@@ -0,0 +1,17 @@
+//
+// Created by Tencent_GO on 2024/4/19.
+//
+
+#ifndef REAL_TIME_RAY_TRACER_CREATESCENE_H
+#define REAL_TIME_RAY_TRACER_CREATESCENE_H
+
+#include "Core/RealTimeRT.h"
+
+
+class Scene;
+inline void createTestScene(Scene &scene) {
+
+}
+
+
+#endif //REAL_TIME_RAY_TRACER_CREATESCENE_H

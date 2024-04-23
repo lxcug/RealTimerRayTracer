b128bb576b54c71851f87c91803f4a6e4fc81dad
lxcug <cugluxu@gmail.com>
2024-04-23 18:49:06 +0800

    All Materials

---
+++ RayTracer/Materials/AllMaterials.h
@@ -0,0 +1,13 @@
+//
+// Created by Tencent_GO on 2024/4/23.
+//
+
+#ifndef REAL_TIME_RAY_TRACER_ALLMATERIALS_H
+#define REAL_TIME_RAY_TRACER_ALLMATERIALS_H
+
+#include "Diffuse.h"
+#include "DiffuseEmissive.h"
+#include "Glossy.h"
+#include "Specular.h"
+
+#endif //REAL_TIME_RAY_TRACER_ALLMATERIALS_H

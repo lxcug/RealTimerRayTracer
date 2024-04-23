9dde3e77f1b2fc6d0d5582df6069bcbffa75f314
lxcug <cugluxu@gmail.com>
2024-04-23 18:49:19 +0800

    All Shapes

---
+++ RayTracer/Shapes/AllShapes.h
@@ -0,0 +1,11 @@
+//
+// Created by Tencent_GO on 2024/4/23.
+//
+
+#ifndef REAL_TIME_RAY_TRACER_ALLSHAPES_H
+#define REAL_TIME_RAY_TRACER_ALLSHAPES_H
+
+#include "Sphere.h"
+#include "Quad.h"
+
+#endif //REAL_TIME_RAY_TRACER_ALLSHAPES_H

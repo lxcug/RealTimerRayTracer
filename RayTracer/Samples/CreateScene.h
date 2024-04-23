2faca79af73a1134b113388535af0d443fe91dd8
lxcug <cugluxu@gmail.com>
2024-04-23 18:53:02 +0800

    Create Scene

--- RayTracer/Samples/CreateScene.h
+++ RayTracer/Samples/CreateScene.h
@@ -5,13 +5,15 @@
 #ifndef REAL_TIME_RAY_TRACER_CREATESCENE_H
 #define REAL_TIME_RAY_TRACER_CREATESCENE_H
 
-#include "Core/RealTimeRT.h"
+#include "Shapes/AllShapes.h"
+#include "Materials/AllMaterials.h"
 
 
 class Scene;
-inline void createTestScene(Scene &scene) {
 
-}
+void createTestScene(Scene &scene);
+
+void createCornellBox(Scene &scene);
 
 
 #endif //REAL_TIME_RAY_TRACER_CREATESCENE_H

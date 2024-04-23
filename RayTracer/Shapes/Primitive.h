5ceb5cdc767ac94331fee8195ae1a593d4affac7
lxcug <cugluxu@gmail.com>
2024-04-23 18:51:53 +0800

    Shapes

--- RayTracer/Shapes/Primitive.h
+++ RayTracer/Shapes/Primitive.h
@@ -8,6 +8,8 @@
 #include "Core/RealTimeRT.h"
 #include "Core/Ray.h"
 #include "Core/Intersection.h"
+#include "glm/gtc/matrix_transform.hpp"
+#include <glm/gtc/quaternion.hpp>
 
 
 class Bounds;
@@ -16,9 +18,29 @@ class Primitive {
 public:
     virtual bool intersect(const Ray &ray, float tMax, Intersection &isect) const = 0;
 
-    [[nodiscard]] virtual bool intersectP(const Ray& r, float tMax) const { return false; }
+    [[nodiscard]] virtual bool intersectP(const Ray &r, float tMax) const { return false; }
 
-    [[nodiscard]] virtual const Bounds &boundingBox() const = 0;
+    [[nodiscard]] virtual Bounds boundingBox() const = 0;
+
+    virtual void setMaterial(const std::shared_ptr<Material> &mat) {}
+
+    [[nodiscard]] virtual const glm::mat4 &getTransform() const {
+        return m_transform;
+    }
+
+    void setTransform(const glm::mat4 &transform) {
+        m_transform = transform;
+    }
+
+    virtual void setTransform(const glm::vec3 &translation, const glm::vec3 &scale = glm::vec3(1.f),
+                              const glm::quat &rotation = glm::identity<glm::quat>()) {
+        m_transform = glm::translate(glm::identity<glm::mat4>(), translation);
+        m_transform = m_transform * glm::mat4_cast(rotation);
+        m_transform = glm::scale(m_transform, scale);
+    }
+
+protected:
+    glm::mat4 m_transform{1.f};
 };
 
 
--- RayTracer/Shapes/Sphere.h
+++ RayTracer/Shapes/Sphere.h
@@ -13,56 +13,29 @@
 
 class Sphere : public Primitive {
 public:
-    Sphere(const glm::point3 &_center, float _radius, std::shared_ptr<Material> _mat)
-            : m_center(_center), m_radius(_radius), m_mat(std::move(_mat)) {
-        m_bounding_box = Bounds(m_center - m_radius, m_center + m_radius);
+    Sphere(float _radius, std::shared_ptr<Material> _mat);
+
+    Sphere(const glm::point3 &_center, float _radius, std::shared_ptr<Material> _mat);
+
+    [[nodiscard]] Bounds boundingBox() const override {
+        glm::point3 center = glm::vec3(m_transform * glm::vec4(m_center, 1.f));
+        auto bounds = Bounds(center - m_radius, center + m_radius);
+
+        return bounds;
     }
 
-    [[nodiscard]] const Bounds & boundingBox() const override {
-        return m_bounding_box;
-    }
+    bool intersect(const Ray &ray, float tMax, Intersection &isect) const override;
 
-    bool intersect(const Ray &ray, float tMax, Intersection &isect) const override {
-        using namespace glm;
-
-        vec3 o = ray.origin - m_center;
-        vec3 d = ray.direction;
-        float a = 1.f;
-        float b = 2 * dot(o, d);
-        float c = o.x * o.x + o.y * o.y + o.z * o.z - m_radius * m_radius;
-
-        float discriminant = b * b - 4 * a * c;
-        if (discriminant < 0.f)
-            return false;
-        auto sqrted = sqrt(discriminant);
-
-        // Find the nearest root that lies in the acceptable range.
-        float root = (-b - sqrted) / (2 * a);
-
-        if (root <= RayPrimitiveIntersectMinTime || root >= tMax) {
-            root = (-b + sqrted) / (2 * a);
-            if (root <= RayPrimitiveIntersectMinTime || root >= tMax)
-                return false;
-        }
-
-        isect.t = root;
-        isect.p = ray.at(root);
-        isect.mat = m_mat;
-        vec3 temp = (isect.p - m_center) / m_radius;
-        isect.normal = dot(temp, ray.direction) < 0 ? temp : -temp;
-
-        // TODO: UV Coordinates
-        isect.u = 0.f;
-        isect.v = 1.f;
-
-        return true;
+    void setMaterial(const std::shared_ptr<Material> &mat) override {
+        m_mat = mat;
     }
 
 private:
-    glm::point3 m_center;
+    // Geometry
+    glm::point3 m_center{0.f};
     float m_radius;
+    // Material
     std::shared_ptr<Material> m_mat;
-    Bounds m_bounding_box;
 };
 
 
